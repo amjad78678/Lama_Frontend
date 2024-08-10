ac93db08f335a9122c37a851c540ac8dd21578c8
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-08-10 12:47:28 +0530

    fixed folder name prblem

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
-import Upload from "./components/projects/upload/Upload";
 import WidgetConfigurations from "./components/projects/widgetConfigurations/WidgetConfigurations";
 import SettingsPage from "./pages/SettingsPage";
 import TranscriptEdit from "./components/projects/transcriptEdit/TranscriptEdit";
 import ProtectedRoute from "./components/common/ProtectedRoute";
+import Upload from "./components/projects/upload/Upload";
 
 function App() {
   return (
