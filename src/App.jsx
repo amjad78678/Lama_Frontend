5fd502850e8c8590cbe43762f00edb9396ae224f
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-08-10 13:51:54 +0530

    Added lazy load for code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ProjectsPage from "./pages/ProjectsPage";
-import WidgetConfigurations from "./components/projects/widgetConfigurations/WidgetConfigurations";
-import SettingsPage from "./pages/SettingsPage";
-import TranscriptEdit from "./components/projects/transcriptEdit/TranscriptEdit";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/common/ProtectedRoute";
-import Upload from "./components/projects/upload/Upload";
+import Loader from "./components/common/Loader";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
+const WidgetConfigurations = lazy(() =>
+  import("./components/projects/widgetConfigurations/WidgetConfigurations")
+);
+const SettingsPage = lazy(() => import("./pages/SettingsPage"));
+const TranscriptEdit = lazy(() =>
+  import("./components/projects/transcriptEdit/TranscriptEdit")
+);
+const Upload = lazy(() => import("./components/projects/upload/Upload"));
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="" element={<ProtectedRoute />}>
-          <Route path="/projects" element={<ProjectsPage />}>
-            <Route path="upload/:projectId" element={<Upload />} />
-            <Route
-              path="transcript/:projectId/:fileId"
-              element={<TranscriptEdit />}
-            />
-            <Route
-              path="widget/:projectId"
-              element={<WidgetConfigurations />}
-            />
-            <Route path="settings/:projectId" element={<SettingsPage />} />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="" element={<ProtectedRoute />}>
+            <Route path="/projects" element={<ProjectsPage />}>
+              <Route path="upload/:projectId" element={<Upload />} />
+              <Route
+                path="transcript/:projectId/:fileId"
+                element={<TranscriptEdit />}
+              />
+              <Route
+                path="widget/:projectId"
+                element={<WidgetConfigurations />}
+              />
+              <Route path="settings/:projectId" element={<SettingsPage />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
